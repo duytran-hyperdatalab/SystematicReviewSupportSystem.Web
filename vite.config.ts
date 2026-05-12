e5b0193377bee3ce10b812d2749f79c2bcf62c75
Duy Trần <141627309+DUUY69@users.noreply.github.com>
2026-05-12 22:41:33 +0700

    fix: rename output files to bust CDN cache

--- vite.config.ts
+++ vite.config.ts
@@ -6,4 +6,12 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: `assets/[name]-[hash]-v2.js`,
+        chunkFileNames: `assets/[name]-[hash]-v2.js`,
+      }
+    }
+  }
 })
